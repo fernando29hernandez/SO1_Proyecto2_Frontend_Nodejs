7c640c0109af574cf5563bcc2ed2464af030766b
Fernando Hernandez <hernandezfernando18982012@gmail.com>
2020-05-10 22:27:14 -0600

    ultimo reporte terminado

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('redis'),
-    clientredis = redis.createClient(6379,"35.237.178.105");
+  clientredis = redis.createClient(6379, "35.237.178.105");
 const mongo = require('mongodb').MongoClient
 const url = 'mongodb://35.237.178.105/'
 var str = [];
-var objetos=[];
+var objetos = [];
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -16,8 +16,7 @@ router.get('/casos', async function (req, res) {
 
   mongo.connect(url, function (err, client) {
     if (err) {
-      console.error(err)
-      return
+      return res.send(str)
     }
     const database = client.db('proyecto')
     const collection = database.collection('casos').aggregate([
@@ -28,9 +27,9 @@ router.get('/casos', async function (req, res) {
         }
       },
       {
-          $sort: {
-            totales: -1
-          }
+        $sort: {
+          totales: -1
+        }
       }
     ])
     //var cursor = collection.find({});
@@ -49,35 +48,62 @@ router.get('/casos', async function (req, res) {
     client.close();
   });
 });
-function insertaredad(edad)
-{
+function insertaredad(edad) {
   objetos.push(edad);
-      
+
 
 }
 router.get('/edades', function (req, res) {
-  
-  clientredis.keys('*', function (err, keys) {
-    if (err){ return console.log(err);}
-    //console.log(keys);
-    objetos=[]
-    for(var i = 0, len = keys.length; i < len; i++) {
-      //console.log(keys[i]);
-      clientredis.get(keys[i], function(err, data){
-        //console.log(JSON.parse(data));
-        var objeto = JSON.parse(data)
-        insertaredad(parseInt(objeto['edad']));
-      });
-    }
-    
-  });
-  
-  console.log(objetos);
-    res.send({response:objetos})
-  //var result = objetos.reduce((r,c) => (r[c] = (r[c] || 0) + 1, r), {})
 
+  clientredis.keys('*', function (err, keys) {
+    if (err) { return res.send({ response: objetos }) } else {
+      //console.log(keys);
+      //objetos=[]
+      let objetos_ = [];
+      for (var i = 0, len = keys.length; i < len; i++) {
+        //console.log(keys[i]);
+        clientredis.get(keys[i], function (err, data) {
+          //console.log(JSON.parse(data));
+          var objeto = JSON.parse(data)
+          objetos_.push(parseInt(objeto['edad']))
+          //insertaredad();
+        });
+      }
+      objetos = objetos_;
+    }
+  });
+  console.log(objetos);
+  res.send({ response: objetos })
+  //var result = objetos.reduce((r,c) => (r[c] = (r[c] || 0) + 1, r), {})
   //console.log(result)
-  
-  
+})
+
+router.get('/ultimo', async function (req, res) {
+  var llave = '';
+  mongo.connect(url, function (err, client) {
+    const database = client.db('proyecto')
+    database.collection('casos', function (err, collection) {
+      collection
+        .find()
+        .sort({ $natural: -1 })
+        .limit(1)
+        .next()
+        .then(
+          function (doc) {
+            llave = doc._id
+            console.log(llave);
+            clientredis.get(''+llave+'', function (err, data) {
+              console.log(data);
+              res.send({ response: JSON.parse(data) })
+            });
+          },
+          function (err) {
+
+          }
+        );
+    });
+    client.close();
+  });
+
 })
 module.exports = router;
